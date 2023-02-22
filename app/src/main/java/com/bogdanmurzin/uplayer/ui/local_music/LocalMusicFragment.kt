1a19e5dce844b51a48e9e2596f7cc41b3119496a
murzinbt@gmail.com <murzinbt@gmail.com>
2023-02-22 16:09:29 +0200

    feat: scratch of local music

--- app/src/main/java/com/bogdanmurzin/uplayer/ui/local_music/LocalMusicFragment.kt
+++ app/src/main/java/com/bogdanmurzin/uplayer/ui/local_music/LocalMusicFragment.kt
@@ -1,11 +1,21 @@
 package com.bogdanmurzin.uplayer.ui.local_music
 
+import android.Manifest
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.bogdanmurzin.uplayer.databinding.FragmentSearchBinding
+
 
 class LocalMusicFragment : Fragment() {
 
@@ -22,5 +32,43 @@ class LocalMusicFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        // TODO require permission
+        if (ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request the permission
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                123)
+        } else {
+            // Permission is already granted
+            // query audio files here
+            getAllAudio()
+        }
     }
+
+    private fun getAllAudio() {
+        val songList = mutableListOf<Song>()
+        val musicResolver: ContentResolver = requireContext().contentResolver
+        val musicUri: Uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        val musicCursor: Cursor? = musicResolver.query(musicUri, null, null, null, null)
+
+        if (musicCursor != null && musicCursor.moveToFirst()) {
+            //get columns
+            val titleColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.TITLE)
+            val idColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media._ID)
+            val artistColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST)
+            //add songs to list
+            do {
+                val thisId = musicCursor.getLong(idColumn)
+                val thisTitle = musicCursor.getString(titleColumn)
+                val thisArtist = musicCursor.getString(artistColumn)
+                songList.add(Song(thisId, thisTitle, thisArtist))
+            } while (musicCursor.moveToNext())
+        }
+
+        musicCursor?.close()
+
+        Log.d("TAGGY", "${songList.size}")
+    }
+
+    data class Song(val id: Long, val title: String, val author: String)
 }
