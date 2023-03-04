2c0e287cd23c1766bd166f31573b8ad9c4d0bd6d
murzinbt@gmail.com <murzinbt@gmail.com>
2023-03-04 16:34:33 +0200

    feat: remove my permissions manager and added easypermissions lib

--- app/src/main/java/com/bogdanmurzin/uplayer/ui/local_music/LocalMusicFragment.kt
+++ app/src/main/java/com/bogdanmurzin/uplayer/ui/local_music/LocalMusicFragment.kt
@@ -1,35 +1,37 @@
 package com.bogdanmurzin.uplayer.ui.local_music
 
-import android.Manifest
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bogdanmurzin.uplayer.R
+import com.bogdanmurzin.uplayer.common.Constants.PERMISSION_REQUEST_CODE
 import com.bogdanmurzin.uplayer.common.Constants.TAG
 import com.bogdanmurzin.uplayer.databinding.FragmentLocalMusicBinding
 import com.bogdanmurzin.uplayer.ui.local_music.adapter.LocalMusicRecyclerViewAdapter
-import com.bogdanmurzin.uplayer.util.PermissionManager
 import com.bogdanmurzin.uplayer.util.extension.RecyclerViewExtension.setDivider
 import com.bogdanmurzin.uplayer.viewmodel.local_music.LocalMusicViewModel
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.DEFAULT_SETTINGS_REQ_CODE
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 
 @AndroidEntryPoint
-class LocalMusicFragment : Fragment(), PermissionManager.PermissionCallback {
+class LocalMusicFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
     private lateinit var binding: FragmentLocalMusicBinding
     private lateinit var recyclerAdapter: LocalMusicRecyclerViewAdapter
     private val viewModel by viewModels<LocalMusicViewModel>()
-
-    @Inject
-    lateinit var permissionManager: PermissionManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,41 +42,38 @@ class LocalMusicFragment : Fragment(), PermissionManager.PermissionCallback {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {}
 
-        // TODO require permission
-//        if (ContextCompat.checkSelfPermission(
-//                requireActivity(),
-//                Manifest.permission.READ_EXTERNAL_STORAGE
-//            )
-//            != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            // Permission is not granted, request the permission
-//            ActivityCompat.requestPermissions(
-//                requireActivity(), arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-//                123
-//            )
-//        } else {
-//            // Permission is already granted
-//
-//            setupRecycler()
-//
-//            viewModel.musicList.observe(viewLifecycleOwner) {
-//                if (it.isNotEmpty()) {
-//                    recyclerAdapter.submitList(it)
-//                }
-//            }
-//
-//            // query audio files here
-//            viewModel.fetchLocalMusic()
-//        }
-//        ------------------------------------------
-        permissionManager = PermissionManager
-            .with(requireActivity())
-            .request(Manifest.permission.READ_EXTERNAL_STORAGE)
-            .setCallbacks(this)
+    override fun onStart() {
+        super.onStart()
+        checkPermissions()
+    }
 
-        permissionManager.ask()
+    private fun checkPermissions() {
+        if (EasyPermissions.hasPermissions(requireContext(), READ_EXTERNAL_STORAGE)) {
+            setupLocalMusic()
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(
+                host = this,
+                rationale = getString(R.string.pmx_default_description),
+                requestCode = PERMISSION_REQUEST_CODE,
+                READ_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    private fun setupLocalMusic() {
+        setupRecycler()
+
+        viewModel.musicList.observe(viewLifecycleOwner) {
+            if (it.isNotEmpty()) {
+                recyclerAdapter.submitList(it)
+            }
+        }
+
+        // query audio files here
+        viewModel.fetchLocalMusic()
     }
 
     private fun setupRecycler() {
@@ -90,35 +89,52 @@ class LocalMusicFragment : Fragment(), PermissionManager.PermissionCallback {
         recyclerView.setDivider(R.drawable.line_divider)
     }
 
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        Log.d(TAG, "onPermissionsDenied: $requestCode :${perms.size}")
+
+        // (Optional) Check whether the user denied any permissions and checked "NEVER ASK AGAIN."
+        // This will display a dialog directing them to enable the permission in app settings.
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            SettingsDialog.Builder(requireActivity()).build().show()
+//
+
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(
+                host = this,
+                rationale = getString(R.string.pmx_default_description),
+                requestCode = PERMISSION_REQUEST_CODE,
+                READ_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        Log.d(TAG, "onPermissionsGranted: $requestCode :${perms.size}")
+        setupLocalMusic()
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        Log.i(TAG, "onRequestPermissionsResult: ")
-        permissionManager.onRequestPermissionResult(
-            requestCode,
-            permissions,
-            grantResults.toTypedArray()
-        )
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.i(TAG, "onRequestPermissionsResult: ${grantResults[0]}")
+        // EasyPermissions handles the request result.
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 
-    override fun onPermissionAccepted(permission: String) {
-        Log.i(TAG, "onPermissionAccepted: $permission")
-        setupRecycler()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == DEFAULT_SETTINGS_REQ_CODE) {
 
-        viewModel.musicList.observe(viewLifecycleOwner) {
-            if (it.isNotEmpty()) {
-                recyclerAdapter.submitList(it)
-            }
+            // Do something after user returned from app settings screen, like showing a Toast.
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.pmx_default_description),
+                LENGTH_LONG
+            ).show()
         }
-
-        // query audio files here
-        viewModel.fetchLocalMusic()
-    }
-
-    override fun onPermissionRejected(permission: String) {
-        Log.i(TAG, "onPermissionRejected: $permission")
-        //TODO("Not yet implemented")
     }
 }
